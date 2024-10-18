9454662ad97514081cf18e9aa266909648efc48b
GreLeonnidas <lmg51@kent.ac.uk>
2024-10-18 12:52:57 +0000

    lol

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,8 @@
-
+x = input("What is your First name")
+for my_char in x:
+    print(my_char, end="")
+    if my_char.islower():
+        print(my_char)
+    if my_char.isupper():
+        print("_",my_char.lower(),end="")
+print()
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,20 @@
+p = 75
+x = 0
+
+allowed_x = [5, 10, 20, 50]
+
+while p > 0:
+
+    x = int(input("Insert coin"))
+    
+    p = p - x 
+    if x > p and p < 0:
+       print("Your change is ",p * (-1))
+    
+    
+
+     
+
+
+
 
